97376d4263f29ded4b78b4511614d6f5426f5400
ostr00000 <ostr00000@gmail.com>
2021-03-14 13:31:13 +0100

    add json help functions

---
+++ lib/pyqt_utils/python/json_serializable.py
@@ -0,0 +1,38 @@
+import json
+import logging
+from typing import Union, Sequence, Type, Callable, Any
+
+logger = logging.getLogger(__name__)
+deepMapElement = Union[None, Type, Callable[[dict], Any]]
+deepMapSequence = Sequence[deepMapElement]
+
+
+def fromJson(jsonStr: str, castTypes: deepMapSequence):
+    if jsonStr == '' and castTypes and castTypes[0]:
+        return castTypes[0]()
+
+    rawObj = json.loads(jsonStr)
+    return deepMap(rawObj, castTypes)
+
+
+def deepMap(obj, mapElements: deepMapSequence):
+    """Iterate over elements and their sub-elements recursively and
+    if element is dict and mapElement for that level is not Node
+    then apply that mapElement to get mapped object."""
+    if not mapElements:  # no more map elements - return original element
+        return obj
+    else:
+        curMap, *otherMaps = mapElements
+
+    if isinstance(obj, dict):
+        if otherMaps:
+            obj = {k: deepMap(v, otherMaps) for k, v in obj.items()}
+        if curMap is not None:
+            return curMap(obj)
+    else:
+        try:
+            return [deepMap(subObj, otherMaps) for subObj in obj]
+        except AttributeError:
+            pass
+
+    return obj
