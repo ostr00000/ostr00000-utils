77ca5e175fe9eaad7fb4e405ee3da6fc6d9e26b0
ostr00000 <ostr00000@gmail.com>
2024-03-17 20:19:04 +0100

    add type var

--- src/pyqt_utils/python/dynamic_loader.py
+++ src/pyqt_utils/python/dynamic_loader.py
@@ -3,27 +3,31 @@ import logging
 import pkgutil
 from inspect import isabstract, isclass
 from types import ModuleType
-from typing import Iterator
+from typing import Iterator, TypeVar
 
 _moduleLogger = logging.getLogger(__name__)
+_T = TypeVar('_T', bound=type)
 
 
 def loadClassFromPackage(
-        package: ModuleType, *,
-        requiredSubclass: type = object,
-        filterPrivate: bool = True,
-        logger: logging.Logger = _moduleLogger,
-) -> Iterator[type]:
+    package: ModuleType,
+    *,
+    requiredSubclass: type[_T] = object,
+    filterPrivate: bool = True,
+    logger: logging.Logger = _moduleLogger,
+) -> Iterator[type[_T]]:
     """
     Load all modules from package and subpackages and return classes defined within.
+
     :param package: Main package from all classes will be loaded.
     :param requiredSubclass: Return only classes with this type.
     :param filterPrivate: Ignore all classes starting with '_' (underscore).
     :param logger: Logger to inform about success or failure.
-    :return:
+    :return: Iterator with classes defined within package.
     """
     for moduleInfo in pkgutil.walk_packages(
-            package.__path__, prefix=package.__name__ + '.'):
+        package.__path__, prefix=package.__name__ + '.'
+    ):
         try:
             mod: ModuleType = importlib.import_module(moduleInfo.name)
         except SyntaxError:
